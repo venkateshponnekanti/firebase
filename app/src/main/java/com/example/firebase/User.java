0b7966bcf18357bb088b17d0772c9e954da3daac
unknown <venkateshponnekanti>
2023-11-20 10:39:02 +0530

    first commit

---
+++ app/src/main/java/com/example/firebase/User.java
@@ -0,0 +1,34 @@
+package com.example.firebase;
+
+public class User {
+    String name,email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    }
+
+
